8059380a800f36aa8867fb4ab93761101ec5be1e
khalilannbiya <syeichkhalil@gmail.com>
2023-09-27 06:16:59 +0700

    feat(models): add protected dates & fillable on some models

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Service extends Model
 {
     use SoftDeletes;
+
+    protected $dates = [
+        "created_at",
+        "updated_at",
+        "deleted_at",
+    ];
+
+    protected $fillable = [
+        "user_id",
+        "title",
+        "description",
+        "delivery_time",
+        "revision_limit",
+        "price",
+        "note",
+    ];
 }
--- app/Models/UserAdvantage.php
+++ app/Models/UserAdvantage.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class UserAdvantage extends Model
 {
-    use HasFactory;
+    use SoftDeletes;
+
+    protected $dates = [
+        "created_at",
+        "updated_at",
+        "deleted_at",
+    ];
+
+    protected $fillable = [
+        "sevice_id",
+        "advantage",
+    ];
 }
